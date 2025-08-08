b2c02f6cbb3621df53d18015ec218c6c2e6f8300
Robin020203 <robin.schildermans@vub.be>
2025-08-08 21:57:28 +0200

    Trying powerup1

--- src/main/scala/game/powerups/DiagonalMove.scala
+++ src/main/scala/game/powerups/DiagonalMove.scala
@@ -11,9 +11,24 @@ class DiagonalMove(power: Piece) extends PowerUp(power) {
 
     // New diagonal rule
     val isDiagonal = math.abs(this.row - toRow) == math.abs(this.column - toColumn) // abs(row diff) = abs(column diff)
-    // TODO: Check path
 
-    // 1 of the 2 rules needs to apply
-    wasValidBefore || isDiagonal
+    // Path clear?
+    def isDiagonalPathClear(): Boolean = {
+      val distance = math.abs(this.row - toRow)
+      // Check direction + define a step (.sign gives +1 if pos and -1 if neg)
+      val rowStep = (toRow - this.row).sign // +1 or -1?
+      val columnStep = (toColumn - this.column).sign // +1 or -1?
+      val range = 1 until distance
+      range.forall {
+        i =>
+          val checkRow = this.row + i * rowStep
+          val checkColumn = this.column + i * rowStep
+          grid.getPiece(checkRow, checkColumn).isEmpty
+      }
+    }
+    
+
+    // Old rule OR Diagonal rule with clear path needs to apply
+    wasValidBefore || (isDiagonal && isDiagonalPathClear())
   }
 }
